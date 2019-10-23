d99fb6e24461ad68d34753763a494d73cc7a6372
hritik <hritikdrocks@gmail.com>
2019-10-23 06:54:32 +0000

    Update

--- build_pie.sh
+++ build_pie.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
-# RAGHU VARMA Build Script 
-# Coded by RV 
+# RAGHU VARMA Build Script
+# Coded by RV
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -26,6 +26,7 @@ PE-SOURCE()
     prebuilts/misc/linux-x86/ccache/ccache -M 50G
     export USE_CCACHE=1
     . build/envsetup.sh
+    make clean
     brunch Dragon
     cd out/target/product/Dragon
     rm -r android-info.txt  dex_bootjars obj_arm  product_copy_files_ignored.txt  symbols boot.img fake_packages ramdisk.img system build_fingerprint.txt gen ramdisk-recovery.img userdata.img build_thumbprint.txt install recovery clean_steps.mk kernel recovery.id data obj previous_build_config.mk  root vendor
